1f0b56a34bade4cd2314ab382929649acd0036a1
Editka Dolejšia <edita@CZNB166.llentab.se>
2019-12-18 13:43:36 +0100

    m

--- src/components/layout.js
+++ src/components/layout.js
@@ -56,14 +56,16 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(30),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          Work from home jobs © {new Date().getFullYear()}
+          <hr />
+          Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby </a>
           and free images downloaded from
